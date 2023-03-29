8c4253d674192d0f0f6164d8db2c599909337a23
Florian Jörg <55591602+florianjoerg@users.noreply.github.com>
2023-03-29 14:15:41 +0200

    Update add_badge.py

--- .github/workflows/add_badge.py
+++ .github/workflows/add_badge.py
@@ -1,20 +1,22 @@
 import glob
 import nbformat
 from nbformat.v4 import new_markdown_cell
+import logging
+logging.setLevel(20) #Info
 
 notebooks = glob.glob("../../docs/notebooks/*.ipynb")
 for notebook in notebooks:
-    print(f"Processing notebook {notebook}")
+    logging.info(f"Processing notebook {notebook}")
     nb = nbformat.read(notebook, nbformat.NO_CONVERT)
     #skip if first cell is not markdown
     if not nb.cells[0].cell_type == "markdown":
-        print("Skipping...")
+        logging.info("Skipping...")
         continue
     source = nb.cells[0].source
     #there is already a badge or button
     if "{{ badge }}" in source or "open in colab" in source.lower():
-        print("Skipping...")
+        logging.info("Skipping...")
         continue
-    print("Inserting {{ badge }}")
+    logging.info("Inserting {{ badge }}")
     nb.cells.insert(0, new_markdown_cell(source='{{ badge }}'))
     nbformat.write(nb, notebook)
